f5ac802cd5264b8c2013a808521340071db30bfa
HSteeleLTS <44376066+HSteeleLTS@users.noreply.github.com>
2019-04-30 08:43:53 -0400

    Update sendLogs.py

--- sendLogs.py
+++ sendLogs.py
@@ -10,6 +10,10 @@
 ########        directory on behalf of the previsouly created Box dev app user
 ########
 ########
+########    This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. 
+########    To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/ 
+########    or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+########    
 ########    Edit script:
 ########
 ########    change the parameters in the commented sections below
