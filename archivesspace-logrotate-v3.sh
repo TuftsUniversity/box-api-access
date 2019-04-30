d7c848dcaba1917f72b638e3c4aca75461eb015b
HSteeleLTS <44376066+HSteeleLTS@users.noreply.github.com>
2019-04-30 08:44:59 -0400

    Update archivesspace-logrotate-v3.sh

--- archivesspace-logrotate-v3.sh
+++ archivesspace-logrotate-v3.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+########    This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. 
+########    To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/ 
+########    or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 log_dir=/usr/local/htdocs/archivesspace/logs
 log_file=${log_dir}\/archivesspace.out
